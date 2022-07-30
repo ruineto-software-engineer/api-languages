3b5cc2d1794f27f87820386651d57923850579e7
Rui Neto <ruineto11@gmail.com>
2022-07-30 01:16:02 -0300

    first commit

---
+++ src/main/java/br/com/alura/api/languages/ApiLanguagesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.api.languages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/api/languages/ApiLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.api.languages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
